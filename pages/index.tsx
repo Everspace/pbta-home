54d8e76ee258421dfc81e252328da5efd1ed9063
Everspace <7560635+Everspace@users.noreply.github.com>
2022-01-22 07:49:55 -0800

    Initial boilerplate

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import { Container, Box, Typography } from "@mui/material"
+import type { NextPage } from "next"
+import Link from "component/Link"
+
+const Home: NextPage = () => {
+  return (
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          MUI v5 + Next.js with TypeScript example
+        </Typography>
+        <Link href="/about" color="secondary">
+          Go to the about page
+        </Link>
+      </Box>
+    </Container>
+  )
+}
+
+export default Home
